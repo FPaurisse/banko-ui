1d30589e2db383e146e000ada3f200dc92ffa680
FPaurisse <paurissefrederic@gmail.com>
2020-10-21 08:36:02 +0200

    remove Element

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { ThemeContextProvider }         from '@providers/theme/useThemeContext';
 import { useTheme }                     from '@providers/theme/useTheme';
 
 import Operations                       from '@components/Operations';
-import { Layout, Accordion, Element }   from '@library/Element';
 
 const client = new GraphQLClient({ url: process.env.API_URL, cache: memCache() })
 
@@ -15,19 +14,15 @@ const App: React.FC = () => {
     const vm = useTheme();
 
     return (
-        <Element main>
-            <ClientContext.Provider value={ client }>
-                <ThemeContextProvider { ...vm }>
-                    <Router>
-                        <Redirect noThrow from='/' to='/operations' />
-                        <Operations path="/operations" />
-                        <Operations path="/operations/:id" />
-                        <Layout path='/layout' />
-                        <Accordion path='/accordion' />
-                    </Router>
-                </ThemeContextProvider>
-            </ClientContext.Provider>
-        </Element>
+        <ClientContext.Provider value={ client }>
+            <ThemeContextProvider { ...vm }>
+                <Router>
+                    <Redirect noThrow from='/' to='/operations' />
+                    <Operations path="/operations" />
+                    <Operations path="/operations/:id" />
+                </Router>
+            </ThemeContextProvider>
+        </ClientContext.Provider>
     );
 };
 
--- src/library/Element/index.tsx
+++
@@ -1,6 +0,0 @@
-import Layout from '@library/Element/examples/Layout';
-import Accordion from '@library/Element/examples/Accordion';
-import Element from '@library/Element/Element';
-import ElementProps from '@library/Element/Element.types';
-
-export { ElementProps, Element, Layout, Accordion };
