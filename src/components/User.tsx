e2fe30f8f620b520beb8db1e54920f1921b2087d
FPaurisse <paurissefrederic@gmail.com>
2020-12-05 17:44:11 +0100

    connect server authent

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,22 +3,32 @@ import { useKeycloak } from '@react-keycloak/web';
 import * as React from 'react';
 
 interface UserInfoModel {
-    preferred_username?: string;
+    sub: string;
+    preferred_username: string;
+    given_name: string;
+    family_name: string;
+    email: string;
+    email_verified: boolean;
+    name: string;
 } 
 
 const User: React.FC<RouteComponentProps> = () => {
     const { keycloak } = useKeycloak();
-    const [userInfo, setUserInfo] = React.useState<UserInfoModel>({});
+    const [userInfo, setUserInfo] = React.useState<UserInfoModel>(null);
 
     React.useEffect(() => {
         keycloak.loadUserInfo().then((userInfo: UserInfoModel) => {
             setUserInfo(userInfo);
         });
     }, [])
+
+    console.log(userInfo)
     
     return (
         <React.Fragment>
-            { userInfo.preferred_username }
+            { userInfo && (
+                <p>{ userInfo.name }</p>
+            ) }
             { keycloak && keycloak.authenticated &&
                     <button onClick={ () => keycloak.logout() }>Logout</button>
             }
--- src/keycloak.ts
+++ src/keycloak.ts
@@ -3,9 +3,9 @@ import Keycloak from 'keycloak-js';
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-    url: 'http://localhost:8080/auth',
-    realm: 'banko-admin',
-    clientId: 'banko-client'
+    url: process.env.KEYCLOAK_URL,
+    realm: process.env.KEYCLOAK_REALM,
+    clientId: process.env.KEYCLOAK_CLIENT_ID
 });
 
 export default keycloak;
